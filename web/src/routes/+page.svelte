931ab451a9b2b895848bbd6ee2d6dade5026f43f
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-04-10 23:57:46 -0400

    Add transitions

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import { m } from '$lib/paraglide/messages.js';
 	import LogLine from '$lib/LogLine.svelte';
+	import { fade } from 'svelte/transition';
 
 	let query = 'SELECT * FROM logs ORDER BY timestamp DESC LIMIT 10';
 	let results: any[] = [];
@@ -20,8 +21,10 @@
 				throw new Error(text || 'Unknown error');
 			}
 			results = await res.json();
-			success = true;
-			setTimeout(() => (success = false), 2000);
+			setTimeout(() => {
+				success = true;
+				setTimeout(() => (success = false), 2500);
+			}, 100);
 		} catch (err) {
 			error = err.message;
 		}
@@ -43,20 +46,29 @@
 			class="w-full rounded border border-gray-600 bg-gray-800 p-2"
 			placeholder="SELECT * FROM logs ORDER BY timestamp DESC LIMIT 100"
 		/>
-		<button on:click={fetchQuery} class="rounded bg-blue-600 px-4 py-2 hover:bg-blue-500">
-			{m.run_query()}
-		</button>
-		{#if error}
-			<div class="rounded border border-red-500 bg-red-900/30 p-2 text-sm text-red-400">
-				<b>Error:</b>
-				{error}
-			</div>
-		{/if}
-		{#if success}
-			<div class="rounded border border-green-500 bg-green-900/30 p-2 text-sm text-green-400">
-				Query successful
-			</div>
-		{/if}
+		<div class="space-between flex gap-1">
+			<button on:click={fetchQuery} class="h-fit rounded bg-blue-600 px-4 py-2 hover:bg-blue-500">
+				{m.run_query()}
+			</button>
+			{#if error}
+				<div
+					class="rounded border border-red-500 bg-red-900/30 p-2 text-sm text-red-400"
+					in:fade={{ duration: 300 }}
+					out:fade={{ duration: 100 }}
+				>
+					<b>Error:</b>
+					{error}
+				</div>
+			{:else if success}
+				<div
+					class="rounded border border-green-500 bg-green-900/30 p-2 text-sm text-green-400"
+					in:fade={{ duration: 300 }}
+					out:fade={{ duration: 200 }}
+				>
+					Query successful
+				</div>
+			{/if}
+		</div>
 
 		<div class="space-y-2 lg:max-h-[75vh] lg:overflow-y-auto">
 			{#each results as log (log)}
