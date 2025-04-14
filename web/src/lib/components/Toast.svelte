8a14a5db6433f427086fc2303ad6ea2f6dc5b5b5
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-04-13 20:32:22 -0400

    Add a toast component

---
+++ web/src/lib/components/Toast.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+	export let text: string;
+	export let duration = 1500;
+	let visible = true;
+
+	setTimeout(() => (visible = false), duration);
+</script>
+
+{#if visible}
+	<div
+		class="rounded bg-black px-2 py-1 text-xs text-white shadow"
+		transition:fly={{ y: -10, duration: 300 }}
+	>
+		{text}
+	</div>
+{/if}
