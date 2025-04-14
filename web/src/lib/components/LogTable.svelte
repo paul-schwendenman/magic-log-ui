e444bdd0c32abdc6d3b5f4c427b2106b0aaeb1a8
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-04-13 21:47:09 -0400

    Refactor logtable to be reactive

---
+++ web/src/lib/components/LogTable.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import ExpandableCell from '$lib/components/ExpandableCell.svelte';
+	import {
+		createColumnHelper,
+		createSvelteTable,
+		FlexRender,
+		getCoreRowModel,
+		renderComponent
+	} from '$lib/table';
+	import type { LogEntry } from '$lib/types';
+    type Props = {
+        logs: LogEntry[]
+    }
+
+    let { logs }: Props = $props();
+    let dataState = $state(logs);
+
+	const colHelp = createColumnHelper<LogEntry>();
+
+	const columnDefs = [
+		colHelp.accessor('timestamp', { header: 'Time' }),
+		colHelp.accessor('trace_id', { header: 'Trace ID' }),
+		colHelp.accessor('level', { header: 'Level' }),
+		colHelp.accessor('message', { header: 'Message' }),
+		colHelp.accessor('raw', {
+			header: 'Raw',
+			cell: ({ cell }) => renderComponent(ExpandableCell, { value: cell.getValue() })
+		})
+	];
+
+	const table = createSvelteTable({
+        get data() {
+            return logs;
+        },
+		columns: columnDefs,
+		getCoreRowModel: getCoreRowModel()
+	});
+</script>
+
+<table class="min-w-full border-collapse border text-sm">
+	<thead>
+		<tr>
+			{#each table.getHeaderGroups() as headerGroup}
+				{#each headerGroup.headers as header}
+					<th class="border px-2 py-1 font-semibold">{header.column.columnDef.header}</th>
+				{/each}
+			{/each}
+		</tr>
+	</thead>
+	<tbody>
+		{#each table.getRowModel().rows as row}
+			<tr>
+				{#each row.getVisibleCells() as cell}
+					<td class="border px-2 py-1">
+						<FlexRender content={cell.column.columnDef.cell} context={cell.getContext()} />
+					</td>
+				{/each}
+			</tr>
+		{/each}
+	</tbody>
+</table>
---
+++ web/src/lib/types.ts
@@ -0,0 +1,7 @@
+export type LogEntry = {
+	timestamp?: string;
+	trace_id?: string;
+	level?: string;
+	message?: string;
+	raw?: any;
+};
