c8e20ba53f9712bf735c5470606dfce1f45e4359
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-04-20 00:48:09 -0400

    Add some more options

--- web/src/lib/time-range-presets.ts
+++ web/src/lib/time-range-presets.ts
@@ -3,10 +3,24 @@ import type { TimeRange } from './types';
 
 export const presets: TimeRange[] = [
 	{
+		label: '5 Minutes',
+		from: sub(new Date(), { minutes: 5 }),
+		to: new Date(),
+		durationMs: 5 * 60_000,
+		live: true
+	},
+	{
 		label: '15 Minutes',
 		from: sub(new Date(), { minutes: 15 }),
 		to: new Date(),
 		durationMs: 15 * 60_000,
+		live: true
+	},
+	{
+		label: '30 Minutes',
+		from: sub(new Date(), { minutes: 30 }),
+		to: new Date(),
+		durationMs: 30 * 60_000,
 		live: true
 	},
 	{
@@ -18,12 +32,50 @@ export const presets: TimeRange[] = [
 		live: true
 	},
 	{
-		label: '1 Day',
+		label: '4 Hours',
+		from: sub(new Date(), { hours: 4 }),
+		to: new Date(),
+		durationMs: 4 * 60 * 60_000,
+		refreshMs: 30_000,
+		live: true
+	},
+	{
+		label: '12 Hours',
+		from: sub(new Date(), { hours: 12 }),
+		to: new Date(),
+		durationMs: 12 * 60 * 60_000,
+		refreshMs: 60_000,
+		live: true
+	},
+	{
+		label: '24 Hours',
 		from: sub(new Date(), { days: 1 }),
 		to: new Date(),
 		durationMs: 24 * 60 * 60_000,
 		refreshMs: 60_000,
 		live: true
-	}
-	// ...
+	},
+	{
+		label: '3 Days',
+		from: sub(new Date(), { days: 3 }),
+		to: new Date(),
+		durationMs: 3 * 24 * 60 * 60_000,
+		refreshMs: 60_000,
+		live: true
+	},
+	// {
+	// 	label: '7 Days',
+	// 	from: sub(new Date(), { days: 7 }),
+	// 	to: new Date(),
+	// 	durationMs: 7 * 24 * 60 * 60_000,
+	// 	refreshMs: 60_000,
+	// 	live: true
+	// },
+	// {
+	// 	label: 'All Time',
+	// 	from: null,
+	// 	to: null,
+	// 	durationMs: undefined,
+	// 	live: false // this one shouldn’t refresh
+	// }
 ];
