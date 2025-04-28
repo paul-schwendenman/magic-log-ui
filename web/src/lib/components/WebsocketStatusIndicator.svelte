b74a08236eb8fac521016e7d5c3501036fb089cd
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-04-28 15:01:22 -0400

    Refactor to expose more websocket states

--- web/src/lib/components/WebsocketStatusIndicator.svelte
+++ web/src/lib/components/WebsocketStatusIndicator.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
 	import { m } from '$lib/paraglide/messages.js';
-	import { isConnected } from '$lib/useWebSocket';
+	import { wsStatus } from '$lib/useWebSocket';
 	import { paused } from '$lib/stores/liveLogs';
 </script>
 
-{#if !$isConnected}
+{#if $wsStatus === 'connecting'}
+	<div class="mb-2 ml-auto inline-block rounded bg-blue-800/50 px-3 py-1 text-sm text-blue-200">
+		{m.zesty_same_kitten_view()}
+	</div>
+{:else if $wsStatus === 'error'}
+	<div class="mb-2 ml-auto inline-block rounded bg-red-900 px-3 py-1 text-sm text-red-300">
+		{m.jumpy_elegant_scallop_cherish()}
+	</div>
+{:else if $wsStatus === 'closed'}
 	<div class="mb-2 ml-auto inline-block rounded bg-red-800/50 px-3 py-1 text-sm text-red-200">
 		{m.known_awake_rooster_mend()}
 	</div>
