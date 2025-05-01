4f4440931c91d35866b965b16a8bce175621264c
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-05-01 00:33:22 -0400

    Format project

--- web/src/lib/components/NavBar.svelte
+++ web/src/lib/components/NavBar.svelte
@@ -12,14 +12,12 @@
 </script>
 
 <nav class="flex items-center gap-6 border-b border-gray-700 bg-gray-900 px-6 py-2 text-sm">
-	<a href="/" class="flex items-center gap-2 text-white font-bold leading-none">
+	<a href="/" class="flex items-center gap-2 leading-none font-bold text-white">
 		<img src="/logo.png" alt="Magic Log logo" class="h-8 w-auto align-middle" />
 	</a>
 
 	{#each links as { href, label }}
-		<a {href}
-			class="text-gray-300 hover:text-white"
-			class:selected={$page.url.pathname === href}>
+		<a {href} class="text-gray-300 hover:text-white" class:selected={$page.url.pathname === href}>
 			{label}
 		</a>
 	{/each}
