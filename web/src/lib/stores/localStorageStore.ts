ee725c042867bf5ff0b1df0673698d1c6c2c77f1
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-05-02 20:38:42 -0400

    Refactor common storage into a helper

--- web/src/lib/stores/localStorageStore.ts
+++ web/src/lib/stores/localStorageStore.ts
@@ -1,23 +1,4 @@
-import { writable, type Writable } from 'svelte/store';
-import { browser } from '$app/environment';
+import { persistentStore } from './persistentStores';
 
-export function localStorageStore<T>(key: string, initial: T): Writable<T> {
-	const store = writable<T>(initial, (set) => {
-		if (browser) {
-			const json = localStorage.getItem(key);
-			if (json !== null) {
-				set(JSON.parse(json));
-			}
-		}
-
-		return () => {};
-	});
-
-	if (browser) {
-		store.subscribe((value) => {
-			localStorage.setItem(key, JSON.stringify(value));
-		});
-	}
-
-	return store;
-}
+export const localStorageStore = <T>(key: string, initial: T) =>
+	persistentStore(key, initial, localStorage);
--- web/src/lib/stores/sessionStorageStore.ts
+++ web/src/lib/stores/sessionStorageStore.ts
@@ -1,18 +1,4 @@
-import { writable, type Writable } from 'svelte/store';
-import { browser } from '$app/environment';
+import { persistentStore } from './persistentStores';
 
-export function sessionStorageStore<T>(key: string, initial: T): Writable<T> {
-	const start = browser
-		? (JSON.parse(sessionStorage.getItem(key) ?? 'null') as T | null) ?? initial
-		: initial;
-
-	const store = writable<T>(start);
-
-	if (browser) {
-		store.subscribe((value) => {
-			sessionStorage.setItem(key, JSON.stringify(value));
-		});
-	}
-
-	return store;
-}
+export const sessionStorageStore = <T>(key: string, initial: T) =>
+	persistentStore(key, initial, sessionStorage);
