5027a64f8d77c52dae28655c57c160f7acdb453d
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-05-06 21:28:28 -0400

    Move old cmd files

--- cmd/config.go
+++ cmd/config.go
@@ -13,13 +13,8 @@ import (
 // configCmd represents the config command
 var configCmd = &cobra.Command{
 	Use:   "config",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Manage configuration settings",
+	Long: ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("config called")
 	},
