8512d94ac1fe5ccf83383f1d95f086f1bbc6119e
Paul Schwendenman <schwendenman.paul@gmail.com>
2025-05-06 23:28:46 -0400

    Add version command

---
+++ cmd/version.go
@@ -0,0 +1,24 @@
+/*
+Copyright © 2025 Paul Schwendenman
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version and exit",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("magic-log version:", version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
